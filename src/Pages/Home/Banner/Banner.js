176d70363d9d0ac2580d8bd17b4ee3cdb3068f07
mjaumi <mjaumi2864@gmail.com>
2022-04-29 11:56:14 +0600

    banner component added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import banner from '../../../images/banner.jpg';
+
+const Banner = () => {
+    return (
+        <div className='w-4/5 mx-auto flex justify-between items-center p-5'>
+            <div className='w-1/2'>
+                <h2 className='text-left text-6xl font-semibold'>Welcome to<br /><span className='font-extrabold text-byteware-light-red text-8xl'>Byteware</span></h2>
+                <p className='text-left mt-5 w-4/5'>The largest laptop warehouse situated in Bangladesh. Where we handle each and every product with care so that we can satisfy our beloved customers.</p>
+            </div>
+            <div className='w-1/2'>
+                <img className='w-[90%] float-right' src={banner} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PageTItle from '../../Shared/PageTitle/PageTItle';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <section>
             <PageTItle title={'Home'} />
-            <h1>This is home</h1>
+            <Banner />
         </section>
     );
 };
