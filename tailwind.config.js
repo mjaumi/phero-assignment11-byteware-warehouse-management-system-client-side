4fd4f659c59e25ddccbaaeaa3b063df57f53fe81
mjaumi <mjaumi2864@gmail.com>
2022-05-01 15:36:00 +0600

    tailwind.config.js file fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'byteware-base-red': '#ff2f23',
-      'byteware-light-red': '#fb4a40',
-      'byteware-white': '#fefcfb',
-      'byteware-dark-gray': '#5f5f6c',
-      'byteware-light-gray': '#f7f7f7',
-      'byteware-border-gray': '#eeeeee',
-      'byteware-footer-bg': '#1d2124',
-      'byteware-genre-bg': '#F2F4F8',
+    extend: {
+      colors: {
+        'byteware-base-red': '#ff2f23',
+        'byteware-light-red': '#fb4a40',
+        'byteware-white': '#fefcfb',
+        'byteware-dark-gray': '#5f5f6c',
+        'byteware-light-gray': '#f7f7f7',
+        'byteware-border-gray': '#eeeeee',
+        'byteware-footer-bg': '#1d2124',
+        'byteware-genre-bg': '#F2F4F8',
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif'],
+      },
+      dropShadow: {
+        'byteware-btn-shadow': '0px 5px 15px rgba(255, 47, 35, 0.4)',
+      },
     },
-    fontFamily: {
-      'poppins': ["'Poppins'", 'sans-serif'],
-    },
-    dropShadow: {
-      'byteware-btn-shadow': '0px 5px 15px rgba(255, 47, 35, 0.4)',
-    },
-    extend: {},
   },
   plugins: [],
 }
